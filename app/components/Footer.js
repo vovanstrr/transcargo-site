6c09ead6b6c6e2464753c6d8e2161e122ee6d821
Vladimir Staroverov <vstarover@gmail.com>
2025-01-30 18:13:34 +0300

    создание header

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,13 +1,18 @@
+import "./footer.css"
 
 const Footer = () => {
     return (
         <footer>
             <div className="footer__container">
-                <p>ООО «ТРАНСКАГО»</p>
-                <p>г. Санкт-Петербург, ул.Новгородская 23</p>
-                <p>Тел. 7 8617 303577</p>
-                <p>Факс 7 8617 303588</p>
-                <p>E-mail: info@transcargo-group.com</p>
+                <p className="footer__margin">ООО «ТРАНСКАГО»</p>
+                
+                <div className="footer__margin">г. Санкт-Петербург, 
+                <p>ул.Новгородская 23</p></div>
+                
+                <div className="footer__margin">Тел. 7 8617 303577
+                <p>Факс 7 8617 303588</p></div>
+                
+                <div>E-mail: info@transcargo-group.com</div>
             </div>
         </footer>
 
--- app/components/Header.js
+++ app/components/Header.js
@@ -1,24 +1,50 @@
+'use client'
+
 import Link from "next/link"
+import Image from "next/image"
+import "./header.css"
 
 const Header = () => {
+
+    const menu = (e) => {
+        console.log('menu ', e);
+                
+    }
+    const menuHidden = (e) => {
+        console.log('menuHidden ', e);
+    }
+    // onMouseMove={menu}
     return (
         <header className="header">
             <div className="header__container">
 
-                <Link href='/'><img src="/logo.png" alt="logo"></img></Link>
-                <nav>
-                    <Link href='/about'>ABOUT US</Link>
-                    <Link href='/fleet'>FLEET</Link>
-                    <Link href='/services'>SERVICES</Link>
-                    <Link href='/news'>NEWS</Link>
-                    <Link href='/gallery'>GALLERY</Link>
-                    <Link href='/contact'>CONTACT US</Link>
-                    {/* <Link href='/users'>Users test link</Link> */}
-                </nav>
-                <div className="header__lang">
-                <Link href='/'><img src="/eng_flag.png" alt="eng"></img></Link>
-                <Link href='/ru'><img src="/rus_flag.png" alt="rus"></img></Link>
+                <div className="header__logo">
+                    <Link href='/'><Image src="/logo.png" alt="logo" width={281} height={66} /></Link>
                 </div>
+
+                <div className="header__lang__nav">
+                    <div className="header__lang">
+                        <Link href='/'><Image src="/eng_flag.png" alt="eng" width={18} height={12} /></Link>
+                        <Link href='/'><Image src="/rus_flag.png" alt="rus" width={18} height={12} /></Link>
+                        {/* <Link href='/ru'><img src="/rus_flag.png" alt="rus"></img></Link> */}
+                    </div>
+
+                    <nav className="header__nav">
+                        <Link href='/about' className="header__link" onMouseOver={menu} onMouseOut={menuHidden}>ABOUT US</Link>
+                        <Image src="/top_menu_razd.png" alt="razdelitel" width={2} height={24} />
+                        <Link href='/fleet' className="header__link">FLEET</Link>
+                        <Image src="/top_menu_razd.png" alt="razdelitel" width={2} height={24} />
+                        <Link href='/services' className="header__link">SERVICES</Link>
+                        <Image src="/top_menu_razd.png" alt="razdelitel" width={2} height={24} />
+                        <Link href='/news' className="header__link">NEWS</Link>
+                        <Image src="/top_menu_razd.png" alt="razdelitel" width={2} height={24} />
+                        <Link href='/gallery'>GALLERY</Link>
+                        <Image src="/top_menu_razd.png" alt="razdelitel" width={2} height={24} />
+                        <Link href='/contact'>CONTACT US</Link>
+                        {/* <Link href='/users'>Users test link</Link> */}
+                    </nav>
+                </div>
+
             </div>
         </header>
 
